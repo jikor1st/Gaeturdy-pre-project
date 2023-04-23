f4bf2b3d1b49ad7772a4186b772cc0935011dda2
Hyunmin Kim <56422158+juventuslandia@users.noreply.github.com>
2023-04-23 18:19:02 +0900

    Initial commit: React 프로젝트 세팅, typescript, styled-components, craco

---
+++ kimhyunmin-todo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CracoTest from '@/components/CracoTest'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <CracoTest></CracoTest>
+    </div>
+  );
+}
+
+export default App;

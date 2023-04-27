34e4dd3bab52f06b68f2ef7b409d1454bb1596df
Hyunmin Kim <56422158+juventuslandia@users.noreply.github.com>
2023-04-27 19:44:51 +0900

    feat. 시맨틱 태그를 활용한 CheckBox 컴포넌트 추가

--- kimhyunmin-todo/src/components/TodoItem.tsx
+++ kimhyunmin-todo/src/components/TodoItem.tsx
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
-import { ItemWrapper, CheckBox, Item, DeleteButton } from "@/styles/TodoItemStyles";
+import { ItemWrapper, Item, DeleteButton } from "@/styles/TodoItemStyles";
 import checkIcon from "@/assets/checkbox.svg";
 import checkNoIcon from "@/assets/checkbox_nocheck.svg";
 import deleteIcon from "@/assets/delete.svg";
 
+const CheckBox = ({ isChecked, handleCheck }: { isChecked: boolean, handleCheck:() => void}) => (
+  <input type="checkbox" checked={isChecked} onChange={handleCheck} />
+);
+
 const TodoItem = () => {
+  const [isChecked, setIsChecked] = useState(false);
 
-    const [isChecked, setIsChecked] = useState(false);
+  const handleCheck = () => {
+    setIsChecked(!isChecked);
+  };
 
-    const handleCheck = () => {
-        setIsChecked(!isChecked);
-    };
-
-    return (
-        <ItemWrapper>
-            <CheckBox src={isChecked ? checkIcon : checkNoIcon} alt="체크 박스" onClick={handleCheck} />
-            <Item isChecked={isChecked}>할 일의 내용</Item>
-            <DeleteButton src={deleteIcon} alt="삭제 버튼" />
-        </ItemWrapper>
-    );
+  return (
+    <ItemWrapper>
+      <CheckBox isChecked={isChecked} handleCheck={handleCheck} />
+      <Item isChecked={isChecked}>할 일의 내용</Item>
+      <DeleteButton src={deleteIcon} alt="삭제 버튼" />
+    </ItemWrapper>
+  );
 };
 
 export default TodoItem;

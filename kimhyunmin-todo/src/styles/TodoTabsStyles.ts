1b317f1a1ae91d8374abcb98050942e35f8d47ee
Hyunmin Kim <56422158+juventuslandia@users.noreply.github.com>
2023-04-27 19:12:19 +0900

    style: TodoTabsStyles.ts 스타일 변경

--- kimhyunmin-todo/src/styles/TodoTabsStyles.ts
+++ kimhyunmin-todo/src/styles/TodoTabsStyles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 const TabWrapper = styled.div<{isActive: boolean}>`
   display: flex;
@@ -6,9 +6,11 @@ const TabWrapper = styled.div<{isActive: boolean}>`
   margin-right: 16px;
   cursor: pointer;
   font-size: 16px;
-  color: ${({isActive}) => isActive ? '#27C8EC' : '#BDBDBD'};
-  font-weight: ${({isActive}) => isActive ? 'bold' : 'normal'};
   position: relative;
+  ${({isActive})=>css`
+      color:${isActive ? '#27C8EC' : '#BDBDBD'};
+      font-weight:${isActive ? 'bold' : 'normal'};
+   `}
 
   &:after {
     content:"";
